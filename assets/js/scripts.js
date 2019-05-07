2d229a1be9de7bf8b7e1c5e5a0888fc46df5e016
Simon-BEE <simonbee1303@gmail.com>
2019-05-07 11:55:16 +0200

    js1

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+var numJoueur = 1;
+var nbColonnes = 5;
+var nbLines = 5;
+var jeu = true;
+var text = "";
+var plateau = [];
+
+for (var i = 0; i < nbLines.length; i++) {
+    plateau[i] += [];
+}
